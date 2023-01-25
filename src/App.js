90a6095ffcd7df5481318c8c61681cc566b77dc4
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-01-25 17:55:10 -0500

    Adding props to a component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,32 @@ function App() {
     <div className="App">
       <div className='principal-container'>
         <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp</h1>
-        <Testimony/>
+        <Testimony
+          name="Shawn Wang"
+          city="Singapore"
+          position="Software Engineer"
+          image=""
+          company="Amazon"
+          text="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        />
+
+        <Testimony
+          name="Sarah Chima"
+          city="Nigeria"
+          position="Software Engineer"
+          image=""
+          company="ChatDesk"
+          text="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+        />
+
+        <Testimony
+          name="Emma Bostian"
+          city="Sweden"
+          position="Software Enginee"
+          image=""
+          company="Spotify"
+          text="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        />
       </div>
     </div>
   );
