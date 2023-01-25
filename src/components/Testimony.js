75508d7a714b6dec71b1f75e41fd268d516defa5
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-01-24 23:45:29 -0500

    Created Testimony component

---
+++ src/components/Testimony.js
@@ -0,0 +1,17 @@
+import React from "react"; /* usa todo el poder de react en el componente */
+
+function Testimony() {
+  return(
+    <div className="testimony-container">
+      <img className="testimony-img" 
+      src={require ("../images/image-2.png")}
+      alt="foto de Emma"/>
+      <div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Testimony;
