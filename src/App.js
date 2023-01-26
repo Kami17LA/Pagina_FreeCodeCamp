c44079de2bc716ebdf723b5088469b0bf672aee6
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-01-26 16:47:07 -0500

    Finished Testimony component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           name="Shawn Wang"
           city="Singapore"
           position="Software Engineer"
-          image=""
+          image="Shawn"
           company="Amazon"
           text="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
         />
@@ -19,7 +19,7 @@ function App() {
           name="Sarah Chima"
           city="Nigeria"
           position="Software Engineer"
-          image=""
+          image="Sarah"
           company="ChatDesk"
           text="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
         />
@@ -28,7 +28,7 @@ function App() {
           name="Emma Bostian"
           city="Sweden"
           position="Software Enginee"
-          image=""
+          image="Emma"
           company="Spotify"
           text="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
         />
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -5,8 +5,8 @@ function Testimony(props) {
   return(
     <div className="testimony-container">
       <img className="testimony-img" 
-      src={require ("../images/image-2.png")}
-      alt="foto de Emma"/>
+      src={require( `../images/image-${props.image}.png`)}
+      alt="student_photo"/>
       <div className="text-testimony-content">
         <p className="name-testimony">{props.name} in {props.city} </p>
         <p className="position-testimony">{props.position} at {props.company}</p>
